0c2d3f5535efd8c7e04338fba5ac4a6531047988
juampivallejo <juampivallejo97@gmail.com>
2024-03-05 00:49:24 -0300

    This not working

---
+++ home-manager/home.nix
@@ -0,0 +1,50 @@
+{ pkgs, username, ... }:
+let
+  homeDirectory = "/home/${username}";
+in
+{
+  imports = [
+    ./neofetch.nix
+    ./wezterm.nix
+  ];
+
+  targets.genericLinux.enable = true;
+
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      warn-dirty = false;
+    };
+  };
+
+  home = {
+    inherit username homeDirectory;
+
+    sessionVariables = {
+      QT_XCB_GL_INTEGRATION = "none"; # kde-connect
+      NIXPKGS_ALLOW_UNFREE = "1";
+      GOPATH = "${homeDirectory}/.local/share/go";
+      GOMODCACHE="${homeDirectory}/.cache/go/pkg/mod";
+    };
+
+    sessionPath = [
+      "$HOME/.local/bin"
+    ];
+  };
+
+  gtk.gtk3.bookmarks = [
+    "file://${homeDirectory}/Documents"
+    "file://${homeDirectory}/Music"
+    "file://${homeDirectory}/Pictures"
+    "file://${homeDirectory}/Videos"
+    "file://${homeDirectory}/Downloads"
+    "file://${homeDirectory}/Desktop"
+    "file://${homeDirectory}/Projects"
+    "file://${homeDirectory}/Vault"
+    "file://${homeDirectory}/.config Config"
+  ];
+
+  programs.home-manager.enable = true;
+  home.stateVersion = "21.11";
+}
