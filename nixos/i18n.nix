4a2d27a1fc2d50bc8235c961874700807af8587d
juampivallejo <juampivallejo97@gmail.com>
2024-03-07 22:38:08 -0300

    Modularization and improvements

---
+++ nixos/i18n.nix
@@ -0,0 +1,27 @@
+{ ... }:
+
+{
+
+  # Configure keymap in X11  [Default config]
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+  # Set your time zone.
+  time.timeZone = "America/Argentina/Mendoza";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_AR.UTF-8";
+    LC_IDENTIFICATION = "es_AR.UTF-8";
+    LC_MEASUREMENT = "es_AR.UTF-8";
+    LC_MONETARY = "es_AR.UTF-8";
+    LC_NAME = "es_AR.UTF-8";
+    LC_NUMERIC = "es_AR.UTF-8";
+    LC_PAPER = "es_AR.UTF-8";
+    LC_TELEPHONE = "es_AR.UTF-8";
+    LC_TIME = "es_AR.UTF-8";
+  };
+}
