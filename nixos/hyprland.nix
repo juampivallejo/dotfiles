263c1941f34689826b532183b618ade78af3f090
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-03-21 20:01:44 -0300

    Push latest updates

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -19,6 +19,10 @@
     opengl.enable = true;
     # Most wayland compositors need this
     nvidia.modesetting.enable = true;
+    # Enable settings menu
+    nvidia.nvidiaSettings = true;
+    # Enabling this to fix suspend issues
+    nvidia.powerManagement.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
