1f9a56f3003e903ccd8d5322f674d19ea4b4eb2f
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-03-28 19:52:54 -0300

    this broke login

---
+++ nixos/default.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./auto-upgrade.nix
+      ./bluetooth.nix
+      ./gnome.nix
+      ./hyprland.nix
+      ./i18n.nix
+      ./sound.nix
+    ];
+}
