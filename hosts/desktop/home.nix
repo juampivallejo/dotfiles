073f454e174ac5a586c5aa1d689267f780722e7b
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-03-29 02:13:48 -0300

    Added modules for home-manager and nix

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -7,9 +7,10 @@ in
     ./desktop-apps.nix
     ./git.nix
     ./gtk.nix
-    ./home-folders.nix
+    ./hyprland.nix
     ./kitty.nix
     ./neofetch.nix
+    ./nvim.nix
     ./packages.nix
     ./tmux.nix
     ./utils.nix
@@ -43,6 +44,7 @@ in
   };
 
   desktopApps.enable = true;
+  hyprland.enable = true;
   neofetch.enable = true;
 
   nixpkgs.config.allowUnfree = true;
