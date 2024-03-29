0ccdb7bcb896d3f62c02b0b2b3b17bb5cb83b391
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-03-29 14:12:23 -0300

    Fixed imports

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -3,20 +3,6 @@ let
   homeDirectory = "/home/${username}";
 in
 {
-  imports = [
-    ./desktop-apps.nix
-    ./git.nix
-    ./gtk.nix
-    ./hyprland.nix
-    ./kitty.nix
-    ./neofetch.nix
-    ./nvim.nix
-    ./packages.nix
-    ./tmux.nix
-    ./utils.nix
-    ./zsh.nix
-  ];
-
   targets.genericLinux.enable = true;
 
   nix = {
