e3dd6e9dbf318e319b853111db8ea6528bda466a
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-06-19 22:00:03 -0300

    Update minor

--- nixos/sound.nix
+++ nixos/sound.nix
@@ -10,7 +10,9 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    wireplumber.enable = true;
+    wireplumber = {
+      enable = true;
+    };
     # If you want to use JACK applications, uncomment this
     # jack.enable = true;
 
