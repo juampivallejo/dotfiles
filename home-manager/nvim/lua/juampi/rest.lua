f4fb879150dcac7bde29a2e1cb6c634f2bafc835
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-08-02 00:24:10 -0300

    Update rest

--- home-manager/nvim/lua/juampi/rest.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set("n", "<leader>hr", "<Plug>RestNvim", { desc = "Run RestNvim" })
-vim.keymap.set("n", "<leader>ha", "<Plug>RestNvimPreview", { desc = "RestNvim Previous" })
-vim.keymap.set("n", "<leader>hd", "<Plug>RestNvimLast", { desc = "Re-run last RestNvim" })
