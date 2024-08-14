34b143cb7bc31f28624f013d98bcf7d840dbb2fe
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-08-14 16:17:09 -0300

    Linting for django html and hyprdim for hyprland

--- home-manager/nvim/lua/plugins/linting.lua
+++ home-manager/nvim/lua/plugins/linting.lua
@@ -3,6 +3,7 @@ local opts = {
   events = { "BufWritePost", "BufReadPost", "InsertLeave" },
   linters_by_ft = {
     -- python = { "mypy" },
+    htmldjango = { "djlint" },
     -- Use the "*" filetype to run linters on all filetypes.
     -- ['*'] = { 'global linter' },
     -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
