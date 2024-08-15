4c70943e813f6d5b29d69895439013c7f28836e6
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-08-15 17:45:41 -0300

    Beatiful spelling config

--- home-manager/nvim/lua/plugins/spelling.lua
+++ home-manager/nvim/lua/plugins/spelling.lua
@@ -1,66 +1,3 @@
-local CSPELL_CONFIG_FILES = {
-  "cspell.json",
-  ".cspell.json",
-  "cSpell.json",
-  ".cspell.json",
-  ".cspell.config.json",
-}
-
----@param filename string
----@param cwd string
----@return string|nil
-local function find_file(filename, cwd)
-  ---@type string|nil
-  local current_dir = cwd
-  local root_dir = "/"
-
-  repeat
-    local file_path = current_dir .. "/" .. filename
-    local stat = vim.loop.fs_stat(file_path)
-    if stat and stat.type == "file" then
-      return file_path
-    end
-
-    current_dir = vim.loop.fs_realpath(current_dir .. "/..")
-  until current_dir == root_dir
-
-  return nil
-end
-
---- Find the first cspell.json file in the directory tree
----@param cwd string
----@return string|nil
-local find_cspell_config_path = function(cwd)
-  for _, file in ipairs(CSPELL_CONFIG_FILES) do
-    local path = find_file(file, cwd or vim.loop.cwd())
-    if path then
-      return path
-    end
-  end
-  return nil
-end
-
---- Find the project .vscode directory, if any
----@param cwd string
----@return string|nil
-local function find_vscode_config_dir(cwd)
-  ---@type string|nil
-  local current_dir = cwd
-  local root_dir = "/"
-
-  repeat
-    local dir_path = current_dir .. "/.vscode"
-    local stat = vim.loop.fs_stat(dir_path)
-    if stat and stat.type == "directory" then
-      return dir_path
-    end
-
-    current_dir = vim.loop.fs_realpath(current_dir .. "/..")
-  until current_dir == root_dir
-
-  return nil
-end
-
 return {
   {
     -- IMPORTANT: cspell must be properly installed (e.g. use npm and verify it can run on cli)
@@ -68,10 +5,46 @@ return {
   },
   {
     "nvimtools/none-ls.nvim",
+    dependencies = { "davidmh/cspell.nvim" },
     opts = function(_, opts)
       local cspell = require("cspell")
-      table.insert(opts.sources, cspell.diagnostics)
-      table.insert(opts.sources, cspell.code_actions)
+      local config = {
+        cspell_config_dirs = { "~/.config/" },
+        -- Includes:
+        -- payload.new_word
+        -- payload.cspell_config_path
+        -- payload.generator_params
+        -- For example, you can format the cspell config file after you add a word
+        on_add_to_json = function(payload)
+            os.execute(
+                string.format(
+                    "jq -S '.words |= sort' %s > %s.tmp && mv %s.tmp %s",
+                    payload.cspell_config_path,
+                    payload.cspell_config_path,
+                    payload.cspell_config_path,
+                    payload.cspell_config_path
+                )
+            )
+        end,
+        ---@param payload AddToDictionarySuccess
+        on_add_to_dictionary = function(payload)
+            -- Includes:
+            -- payload.new_word
+            -- payload.cspell_config_path
+            -- payload.generator_params
+            -- payload.dictionary_path
+            -- For example, you can sort the dictionary after adding a word
+            os.execute(
+                string.format(
+                    "sort %s -o %s",
+                    payload.dictionary_path,
+                    payload.dictionary_path
+                )
+            )
+        end
+      }
+      table.insert(opts.sources, cspell.diagnostics.with({ config = config }))
+      table.insert(opts.sources, cspell.code_actions.with({ config = config }))
       opts.fallback_severity = vim.diagnostic.severity.HINT
     end,
   },
