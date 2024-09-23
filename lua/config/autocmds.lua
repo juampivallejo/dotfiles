333bb1c233d47a4338bbd71914aef87c7bd991cb
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-09-23 13:22:21 -0300

    push updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,3 +4,11 @@
 --
 local autocmd_group = vim.api.nvim_create_augroup("Custom auto-commands", { clear = true })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
+  callback = function(event)
+    local opts = { buffer = event.buf, silent = true }
+    vim.keymap.set('n', '<C-n>', '<cmd>cn | wincmd p<CR>zz', opts)
+    vim.keymap.set('n', '<C-p>', '<cmd>cN | wincmd p<CR>zz', opts)
+  end,
+})
