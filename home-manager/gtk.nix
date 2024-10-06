816de3e48151e790b369cda834fc16d90b3a90f1
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-10-06 03:15:01 +0000

    We got WSL working

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -1,18 +1,20 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs;
-    [
-      arc-theme # gtk theme
-    ];
-  gtk = {
-    enable = true;
-    iconTheme = {
-      name = "Papirus";
-      package = pkgs.papirus-icon-theme;
-    };
+{ lib, config, pkgs, ... }: {
+  config = lib.mkIf config.desktopApps.enable {
+    home.packages = with pkgs;
+      [
+        arc-theme # gtk theme
+      ];
+    gtk = {
+      enable = true;
+      iconTheme = {
+        name = "Papirus";
+        package = pkgs.papirus-icon-theme;
+      };
 
-    theme = {
-      name = "Arc-Dark";
-      package = pkgs.arc-theme;
+      theme = {
+        name = "Arc-Dark";
+        package = pkgs.arc-theme;
+      };
     };
   };
 }
