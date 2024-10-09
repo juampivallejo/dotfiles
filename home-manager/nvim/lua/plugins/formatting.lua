a78bc234dc119851f09712c91fdfe086fa9b786e
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-10-09 03:58:48 +0000

    Fix formatting

--- home-manager/nvim/lua/plugins/formatting.lua
+++ home-manager/nvim/lua/plugins/formatting.lua
@@ -1,46 +1,48 @@
-local M = {}
+local opts = function()
+  ---@type conform.setupOpts
+  local opts = {
+    default_format_opts = {
+      timeout_ms = 3000,
+      async = false, -- not recommended to change
+      quiet = false, -- not recommended to change
+      lsp_format = "fallback", -- not recommended to change
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      sh = { "shfmt" },
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      css = { "prettierd" },
+      html = { "prettierd" },
+      htmldjango = { "djlint" },
+      json = { "prettierd" },
+      yaml = { "prettierd" },
+      markdown = { "prettierd" },
+      graphql = { "prettierd" },
+      python = { "isort", "ruff_format" },
+      go = { "gofmt" },
+      nix = { "nixfmt" },
+    },
+    ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+      -- # Example of using dprint only when a dprint.json file is present
+      -- dprint = {
+      --   condition = function(ctx)
+      --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+      --   end,
+      -- },
+      --
+      -- # Example of using shfmt with extra args
+      shfmt = {
+        prepend_args = { "-i", "2", "-ci" }, -- -i 2 (2 spaces) and --ci (case-indent: switch cases will be indented)
+      },
+    },
+  }
+  return opts
+end
+
 return {
   "stevearc/conform.nvim",
-  dependencies = { "mason.nvim" },
-  lazy = true,
-  cmd = "ConformInfo",
-  keys = {
-    {
-      "<leader>cF",
-      function()
-        require("conform").format({ formatters = { "injected" } })
-      end,
-      mode = { "n", "v" },
-      desc = "Format Injected Langs",
-    },
-  },
-  opts = function()
-    ---@class ConformOpts
-    local opts = {
-      -- LazyVim will use these options when formatting with the conform.nvim formatter
-      default_format_opts = {
-        timeout_ms = 3000,
-        async = false, -- not recommended to change
-        quiet = false, -- not recommended to change
-      },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        sh = { "shfmt" },
-        javascript = { "prettierd" },
-        typescript = { "prettierd" },
-        css = { "prettierd" },
-        html = { "prettierd" },
-        htmldjango = { "djlint" },
-        json = { "prettierd" },
-        yaml = { "prettierd" },
-        markdown = { "prettierd" },
-        graphql = { "prettierd" },
-        python = { "isort", "ruff_format" },
-        go = { "gofmt" },
-        nix = { "nixfmt" },
-      },
-    }
-    return opts
-  end,
-  config = M.setup,
+  opts = opts(),
 }
