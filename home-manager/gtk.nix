b347e967633bcfa20751a6001405943982293da5
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-10-25 02:35:00 -0300

    even more updates to hypr

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -1,9 +1,18 @@
 { lib, config, pkgs, ... }: {
   config = lib.mkIf config.desktopApps.enable {
-    home.packages = with pkgs;
-      [
-        arc-theme # gtk theme
-      ];
+    home.packages = with pkgs; [
+      arc-theme # gtk theme
+      rose-pine-icon-theme
+    ];
+
+    # FIXME: Cursor on GTK apps not working
+    home.pointerCursor = {
+      gtk.enable = true;
+      package = pkgs.rose-pine-icon-theme;
+      name = "Rose-Pine";
+      size = 24;
+    };
+
     gtk = {
       enable = true;
       iconTheme = {
