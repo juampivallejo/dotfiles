a30ee16b9d1e781adc157ed08480e3fdd96c13e3
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-10-25 01:08:36 -0300

    Major hyprland rewrite

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -34,12 +34,11 @@
   xdg = {
     portal = {
       enable = true;
-      extraPortals = with pkgs;
-        [
-          xdg-desktop-portal-wlr
-          # xdg-desktop-portal-gtk
-        ];
-      gtkUsePortal = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-hyprland
+      ];
     };
   };
 
