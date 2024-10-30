9d992d2d3033bc6484031a9ef4409a5a9dff4ba6
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-10-29 23:50:00 -0300

    waybar wip working

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -14,13 +14,25 @@
       recursive = true;
       executable = true;
     };
+    home.file."./.config/waybar/" = {
+      source = ./waybar;
+      recursive = true;
+    };
+    home.file."./.config/dunst/" = {
+      source = ./dunst;
+      recursive = true;
+    };
 
     home.packages = with pkgs; [
       pkgs-unstable.waybar
+      killall # Used in some scripts
       pavucontrol # Audio control with waybar
+      dunst # notifications
+      networkmanagerapplet # (nm-applet) Network and wifi sys tray
       swww # Wallpapers
       libnotify
       slurp # for screenshots
+      mpc-cli # Music Player control cli
     ];
   };
 }
