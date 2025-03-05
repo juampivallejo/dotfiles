6dc13b721327ae8cdb44555ddc339df8aa5ed585
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2025-03-05 11:44:09 -0300

    Tmux color

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,11 +4,18 @@
 --
 local autocmd_group = vim.api.nvim_create_augroup("Custom auto-commands", { clear = true })
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'qf',
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
   callback = function(event)
     local opts = { buffer = event.buf, silent = true }
-    vim.keymap.set('n', '<C-n>', '<cmd>cn | wincmd p<CR>zz', opts)
-    vim.keymap.set('n', '<C-p>', '<cmd>cN | wincmd p<CR>zz', opts)
+    vim.keymap.set("n", "<C-n>", "<cmd>cn | wincmd p<CR>zz", opts)
+    vim.keymap.set("n", "<C-p>", "<cmd>cN | wincmd p<CR>zz", opts)
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "sql", "dbout" },
+  callback = function()
+    vim.bo.commentstring = "-- %s"
   end,
 })
