f8aa6db6bee6e864208377be13659ad88b6beb5b
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2025-05-05 19:58:13 -0700

    fix darwin build

--- home-manager/packages/work.nix
+++ home-manager/packages/work.nix
@@ -1,14 +1,15 @@
 { config, lib, pkgs, ... }:
 let
-  workPackages = with pkgs; [
-    # Beanstalk CLI
-    awsebcli
-    # Work
-    aws-azure-login
-  ];
+  workPackages = with pkgs;
+    [
+      # Beanstalk CLI
+      awsebcli
+    ];
   extras = with pkgs;
     lib.optionals config.isNixOS [
       ssm-session-manager-plugin # For ECS ssh
+      # Work
+      aws-azure-login
     ];
 
 in { home.packages = workPackages ++ extras; }
