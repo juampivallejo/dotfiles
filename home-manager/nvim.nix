388c77051341748e05289b4d6dd77e205e118093
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2025-05-05 21:58:46 -0300

    fix darwin build

--- home-manager/nvim.nix
+++ home-manager/nvim.nix
@@ -5,25 +5,28 @@
     recursive = true;
   };
 
-  home.packages = with pkgs; [
-    # Neovim LSPs & Formatters
-    pyright
-    # nodePackages.cspell # Spelling Linter
-    lua-language-server # Lua LSP
-    stylua # Lua formatter
-    ruff # Python formatter
-    isort # Python Sort
-    nil # Nix LSP
-    nixfmt-classic # Nix format
-    prettierd # Formatter
-    shfmt # Shell formatter
-    djlint # Django linter and formatter
-    # Neovim + SQL
-    # postgresql # To have psql executable
-    sleek # SQL formatter
-    sqls # LSP
-    copilot-language-server
-  ];
+  home.packages = with pkgs;
+    [
+      # Neovim LSPs & Formatters
+      pyright
+      lua-language-server # Lua LSP
+      stylua # Lua formatter
+      ruff # Python formatter
+      isort # Python Sort
+      nil # Nix LSP
+      nixfmt-classic # Nix format
+      prettierd # Formatter
+      shfmt # Shell formatter
+      djlint # Django linter and formatter
+      # Neovim + SQL
+      sleek # SQL formatter
+      sqls # LSP
+    ] ++ (if config.isNixOS then [
+      copilot-language-server-fhs
+      postgresql
+      nodePackages.cspell # Spelling Linter
+    ] else
+      [ ]) ++ (if config.isDarwin then [ copilot-language-server ] else [ ]);
 
   programs.neovim = {
     enable = true;
