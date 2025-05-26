ed86f5b73f1295abacc83885159f1ec903f8fe13
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2025-05-25 21:58:03 -0300

    Update and fix awsebcli

--- home-manager/packages/work.nix
+++ home-manager/packages/work.nix
@@ -1,10 +1,10 @@
 { config, lib, pkgs, pkgs-old, ... }:
 let
-  workPackages = with pkgs;
-    [
-      # Beanstalk CLI
-      pkgs-old.awsebcli
-    ];
+  workPackages = with pkgs; [
+    # Beanstalk CLI
+    python312Packages.packaging # Fix awsebcli packaging bug
+    awsebcli
+  ];
   extras = with pkgs;
     lib.optionals config.isNixOS [
       ssm-session-manager-plugin # For ECS ssh
