bb2bf1703b4596a154ae56b50f6daf3d1ab8b301
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2025-07-08 23:35:50 -0300

    updates

--- home-manager/nvim/lua/plugins/code-companion.lua
+++ home-manager/nvim/lua/plugins/code-companion.lua
@@ -1,8 +1,9 @@
-return {
-  "olimorris/codecompanion.nvim",
-  opts = {},
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-}
+return {}
+-- return {
+--   "olimorris/codecompanion.nvim",
+--   opts = {},
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "nvim-treesitter/nvim-treesitter",
+--   },
+-- }
--- home-manager/nvim/lua/plugins/copilot-config.lua
+++ home-manager/nvim/lua/plugins/copilot-config.lua
@@ -1,19 +1,20 @@
 -- lua/plugins/copilot.lua
-return {
-  "zbirenbaum/copilot.lua",
-  enabled = true,
-  keys = {
-    {
-      "<leader>at",
-      function()
-        if require("copilot.client").is_disabled() then
-          require("copilot.command").enable()
-        else
-          require("copilot.command").disable()
-        end
-      end,
-      -- LazyVim toggle does not work, but status line has an Icon when enabled at right bottom
-      desc = "Toggle (Copilot)",
-    },
-  },
-}
+return {}
+-- return {
+--   "zbirenbaum/copilot.lua",
+--   enabled = true,
+--   keys = {
+--     {
+--       "<leader>at",
+--       function()
+--         if require("copilot.client").is_disabled() then
+--           require("copilot.command").enable()
+--         else
+--           require("copilot.command").disable()
+--         end
+--       end,
+--       -- LazyVim toggle does not work, but status line has an Icon when enabled at right bottom
+--       desc = "Toggle (Copilot)",
+--     },
+--   },
+-- }
