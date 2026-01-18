7a69c095c60439621d2116c5da5f0f39bc584626
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2026-01-18 20:11:52 -0300

    update

--- home-manager/nvim/lua/plugins/picker.lua
+++ home-manager/nvim/lua/plugins/picker.lua
@@ -8,12 +8,14 @@ return {
           keys = {
             ["<a-.>"] = { "toggle_hidden", mode = { "i", "n" } },
             ["<a-,>"] = { "toggle_ignored", mode = { "i", "n" } },
+            ["<a-g>"] = { "toggle_live", mode = { "i", "n" } },
           },
         },
         list = {
           keys = {
             ["<a-.>"] = { "toggle_hidden", mode = { "i", "n" } },
             ["<a-,>"] = { "toggle_ignored", mode = { "i", "n" } },
+            ["<a-g>"] = { "toggle_live", mode = { "i", "n" } },
           },
         },
       },
